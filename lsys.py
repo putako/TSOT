8dfeed41f8ac65a7afb949437f524dddf43eb0ce
Rigormortis115 <thegame1151337@gmail.com>
2018-03-03 09:37:37 -0500

    Add files via upload

--- lsys.py
+++ lsys.py
@@ -1,219 +1,214 @@
 import random
 
-def widenNetworks(lhch):   # Rule 1
-  rnum = random.randint(0, 100)
-  rhstr = ""
-  if lhch == 'N':
-    if rnum < 10:
-      rhstr = '[N][N]'
-    elif rnum < 70:
-      rhstr = '[N][N][N]'
-    else:
-      rhstr = '[N][N][N][N]'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr    
-        
-def populateBaseNetwork(lhch):   # Rule 2
-  rnum = random.randint(0, 100)
-  rhstr = ""        
-  if lhch == 'N':
-    if rnum < 10:
-      rhstr = '#'
-    elif rnum < 30:
-      rhstr = '##'
-    elif rnum < 80:
-      rhstr = '###'
-    else:
-      rhstr = '#####'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
-    
-def deepenNetworks(lhch):   # Rule 3
-  rnum = random.randint(0, 100)
-  rhstr = ""
-  if lhch == '#':
-    if rnum < 60:
-      rhstr = '#'
-    elif rnum < 65:
-      rhstr = '#<#>'
-    elif rnum < 70:
-      rhstr = '#<##>'
-    elif rnum < 85:
-      rhstr = '#<###>'
-    else:
-      rhstr = '#<#####>'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
-  
-def stringifyNetworks(lhch):   # Rule 4
-  rnum = random.randint(0, 100)
-  rhstr = ""
-  if lhch == '#':
-    if rnum < 80:
-      rhstr = '#'
-    elif rnum < 90:
-      rhstr = '#<#>'
-    else:
-      rhstr = '#<#<#>>'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
-  
-def createEndpoints(lhch):   # Rule 5
-  rnum = random.randint(0, 100)
-  rhstr = ""
-  if lhch == '#':
-    if rnum < 30:
-      rhstr = 'E'
-    else:
-      rhstr = '#'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
-  
-def capEndpoints(lhch):   # Rule 6
-  rhstr = ""
-  if lhch == '#':
-    rhstr = 'E'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
-  
-def vulnerablizeEndpoints(lhch):   # Rule 7
-  rnum = random.randint(0, 100)
-  rhstr = ""
-  if lhch == 'E':
-    if rnum < 60:
-      rhstr = 'E'
-    elif rnum < 70:
-      rhstr = 'E{V}'
-    elif rnum < 80:
-      rhstr = 'E{VV}'
-    elif rnum < 90:
-      rhstr = 'E{VVV}'
-    else:
-      rhstr = 'E{VVVVV}'
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
-  
-def finalizeVulnerabilities(lhch):   # Rule 8
-  rnum = str(random.randint(0, 100))
-  # add in stuff to make sure you don't get the same vulnerability twice.
-  rhstr = ""
-  if lhch == 'V':
-    rhstr = rnum + ","
-  else:
-    rhstr = lhch
-  #print rhstr
-  return rhstr
+class Lsys:
+	def widenNetworks(lhch):   # Rule 1
+		rnum = random.randint(0, 100)
+		rhstr = ""
+		if lhch == 'N':
+			if rnum < 10:
+				rhstr = '[N][N]'
+			elif rnum < 70:
+				rhstr = '[N][N][N]'
+			else:
+				rhstr = '[N][N][N][N]'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr    
+			
+	def populateBaseNetwork(lhch):   # Rule 2
+		rnum = random.randint(0, 100)
+		rhstr = ""        
+		if lhch == 'N':
+			if rnum < 30:
+				rhstr = '###'
+			elif rnum < 80:
+				rhstr = '####'
+			else:
+				rhstr = '#####'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
+		
+	def deepenNetworks(lhch):   # Rule 3
+		rnum = random.randint(0, 100)
+		rhstr = ""
+		if lhch == '#':
+			if rnum < 60:
+				rhstr = '#'
+			elif rnum < 65:
+				rhstr = '#<#>'
+			elif rnum < 70:
+				rhstr = '#<##>'
+			elif rnum < 85:
+				rhstr = '#<###>'
+			else:
+				rhstr = '#<#####>'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
+	  
+	def stringifyNetworks(lhch):   # Rule 4
+		rnum = random.randint(0, 100)
+		rhstr = ""
+		if lhch == '#':
+			if rnum < 80:
+				rhstr = '#'
+			elif rnum < 90:
+				rhstr = '#<#>'
+			else:
+				rhstr = '#<#<#>>'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
+	  
+	def createEndpoints(lhch):   # Rule 5
+		rnum = random.randint(0, 100)
+		rhstr = ""
+		if lhch == '#':
+			if rnum < 30:
+				rhstr = 'E'
+			else:
+				rhstr = '#'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
+	  
+	def capEndpoints(lhch):   # Rule 6
+		rhstr = ""
+		if lhch == '#':
+			rhstr = 'E'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
+	  
+	def vulnerablizeEndpoints(lhch):   # Rule 7
+		rnum = random.randint(0, 100)
+		rhstr = ""
+		if lhch == 'E':
+			if rnum < 60:
+				rhstr = 'E'
+			elif rnum < 70:
+				rhstr = 'E{V}'
+			elif rnum < 80:
+				rhstr = 'E{VV}'
+			elif rnum < 90:
+				rhstr = 'E{VVV}'
+			else:
+				rhstr = 'E{VVVVV}'
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
+	  
+	def finalizeVulnerabilities(lhch):   # Rule 8
+		rnum = str(random.randint(0, 100))
+		# add in stuff to make sure you don't get the same vulnerability twice.
+		rhstr = ""
+		if lhch == 'V':
+			rhstr = rnum + ","
+		else:
+			rhstr = lhch
+		#print rhstr
+		return rhstr
 
-def processString1(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + widenNetworks(ch)
-  #print newstr
-  return newstr
+	def processString1(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + widenNetworks(ch)
+		#print newstr
+		return newstr
 
-def processString2(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + populateBaseNetwork(ch)
-  #print newstr
-  return newstr
+	def processString2(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + populateBaseNetwork(ch)
+		#print newstr
+		return newstr
 
-def processString3(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + deepenNetworks(ch)
-  #print newstr
-  return newstr
-  
-def processString4(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + stringifyNetworks(ch)
-  #print newstr
-  return newstr
-  
-def processString5(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + createEndpoints(ch)
-  #print newstr
-  return newstr
-  
-def processString6(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + capEndpoints(ch)
-  #print newstr
-  return newstr
-  
-def processString7(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + vulnerablizeEndpoints(ch)
-  #print newstr
-  return newstr
-  
-def processString8(oldStr):
-  newstr = ""
-  for ch in oldStr:
-    newstr = newstr + finalizeVulnerabilities(ch)
-  #print newstr
-  return newstr
+	def processString3(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + deepenNetworks(ch)
+		#print newstr
+		return newstr
+	  
+	def processString4(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + stringifyNetworks(ch)
+		#print newstr
+		return newstr
+	  
+	def processString5(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + createEndpoints(ch)
+		#print newstr
+		return newstr
+	  
+	def processString6(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + capEndpoints(ch)
+		#print newstr
+		return newstr
+	  
+	def processString7(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + vulnerablizeEndpoints(ch)
+		#print newstr
+		return newstr
+	  
+	def processString8(oldStr):
+		newstr = ""
+		for ch in oldStr:
+			newstr = newstr + finalizeVulnerabilities(ch)
+		#print newstr
+		return newstr
 
-def createLSystem(numIters,axiom):
-  startString = axiom
-  endString = ""
-  
-  endString = endString + processString1(startString)
-  startString = endString
-  endString = ""
-  
-  endString = endString + processString2(startString)
-  startString = endString
-  endString = ""
-  
-  for i in range(numIters):
-	endString = endString + processString3(startString)
-	startString = endString
-	endString = ""
-	
-	endString = endString + processString4(startString)
-	startString = endString
-	endString = ""
-	
-	endString = endString + processString5(startString)
-	startString = endString
-	endString = ""
-	
-  endString = endString + processString6(startString)
-  startString = endString
-  endString = ""
-  
-  endString = endString + processString7(startString)
-  startString = endString
-  endString = ""
-  
-  endString = endString + processString8(startString)
-  startString = endString
-  endString = ""
-  
-  print startString
-  print "done now"
+	def __init__(self,numIters,axiom):
+		startString = axiom
+		endString = ""
 
-print "Enter a lsysIter:",
-lsysIter = int(raw_input())
-createLSystem(lsysIter, 'N')
+		endString = endString + __processString1(startString)
+		startString = endString
+		endString = ""
+
+		endString = endString + __processString2(startString)
+		startString = endString
+		endString = ""
+
+		for i in range(numIters):
+			endString = endString + __processString3(startString)
+			startString = endString
+			endString = ""
+
+			endString = endString + __processString4(startString)
+			startString = endString
+			endString = ""
+
+			endString = endString + __processString5(startString)
+			startString = endString
+			endString = ""
+		
+		endString = endString + __processString6(startString)
+		startString = endString
+		endString = ""
+
+		endString = endString + __processString7(startString)
+		startString = endString
+		endString = ""
+
+		endString = endString + __processString8(startString)
+		startString = endString
+		endString = ""
+	  
+		return startString
+		print "done now"
--- lsysIn.py
+++ lsysIn.py
@@ -1,12 +1,5 @@
-
 #import stuff
-import random, os, errno
-
-currentNetwork = 'a'
-charList = ""
-networkList = []
-numNetworks = 0
-numHosts = 0
+import os, errno, lsys
 
 # generate home dir
 try:
@@ -14,66 +7,85 @@ try:
 except OSError as e:
   if e.errno != errno.EEXIST:
     raise
-
-# define code to generate full subnet block
-def genNetwork():
-	global numNetworks
-	numNetworks += 1
-	currentNetwork = "network" + str(numNetworks)
-	networkList.append(currentNetwork)
-	return currentNetwork
-
-# generate other networks
-def genEnterprise(charList):
-	for x in charList:
-		if x == '[': # [ , generate new network
-			network = genNetwork()
-			relevantNetwork = network
-			print "There was a [! Spawn a network!"
-		elif x == '<': # < , generate new stubNetwork
-			network2 = genNetwork()
-			relevantNetwork = network2
-			print "There was a <! Spawn a network!"
-		elif x == '$': # $ , generate new serialNetwork
-			network3 = genNetwork()
-			relevantNetwork = network3
-			print "There was a $! Spawn a network!"
-		elif x == '#': # # , generate new host
+	
+# Generate information about networks from charList
+def dockerWriter(charList):
+	# Declare necessary variables
+	networkID = []
+	networkPrint = []
+	netNum = 0
+	machNum = 0
+	
+	# Open files
+	f = open('home/docker-compose.yml', 'a')
+	
+	# Write static header
+	f.write('version: "3"\n')
+	
+	# Don't look down....
+	for index, x in enumerate(charList):
+		if x == '[': # IF we see a [ at x index, do rule
+			networkID.append(netNum)
+			networkPrint.append(netNum)
+			netNum += 1
+			print(networkID)
+		elif x == '<': # IF we see a < at x index, do rule
+			networkID.append(netNum)
+			networkPrint.append(netNum)
+			netNum += 1
+			print(networkID)
+		elif x == ']':
+			networkID = networkID[:-1]
+			print(networkID)
+		elif x == '>':
+			networkID = networkID[:-1]
+			print(networkID)
+		elif x == 'E':
+			#### DOCKER SPECIFIC FILE STUFF ####
 			# generate folder for docker
-			global numHosts
-			numHosts += 1
-			machineId = relevantNetwork + str(numHosts)
+			
+			
+			if charList[index - 1] == '<':
+				machineName = "device_" + str(machNum)
+			else:
+				machineName = "machine_" + str(machNum)
+				
 			try:
-			  os.makedirs("home\\" + machineId)
+			  os.makedirs("home/" + machineName)
 			except OSError as e:
 			  if e.errno != errno.EEXIST:
 				raise
-			# put stuff in the file here
-			f = open('home\\' + machineId + '\\Dockerfile', 'a')
-			f.write("FROM alpine\nCMD ['top']")
-			f.close()
 			
-			f = open('home\\docker-compose.yml', 'a')
-			f.write('''  host%s%s:
-			build: .\\%s
-			image: %s
-			networks:
-			  - %s\n''' % ("host", machineId, machineId, machineId, relevantNetwork))
-			f.close()
-		else: # skip bullshit
-			print "nothing"
-
-# test stuff
-var = "[#<#><#>$#<#>$#<#$#<#>>][#<#$#>$#$#<#$#>$#<#$#<#>>][#<#><#>]"
-genEnterprise(var)
+			# put stuff in the file here
+			e = open('home/' + machineName + '/Dockerfile', 'a')
+			e.write("FROM alpine\nCMD ['top']")
+			e.close()
+				
+			# iterate machNum
+			machNum += 1
+			
+			#### DOCKER COMPOSE FILE STUFF ####
+			f.write('  host%s:\n' % machineName)
+			f.write('    build: ./%s\n' % machineName)
+			f.write('    image: %s\n' % machineName)
+			f.write('    networks:\n')
+			f.write('      - %s\n' % ("network" + str(networkID[-1])))
+			if charList[index - 1] == '<':
+				f.write('      - %s\n' % ("network" + str(networkID[-2])))
+		else:
+			pass
 	
-# print network info to file
-f = open('home\\docker-compose.yml', 'a')
-f.write("\n################ NETWORKING ################\n")
-f.write('networks:\n')
-for x in networkList:
-  f.write('''
-  %s:
-    driver: bridge
-''' % x)
-f.close()
+	# Write networking info
+	f.write("\n################ NETWORKING ################\n")
+	f.write("networks:\n\n")
+	
+	for x in networkPrint:
+		f.write("  network%s:\n" % x)
+		f.write("    driver: bridge\n\n")
+			
+	# Close files
+	f.close()
+
+charList = lsys.Lsys(1, "N")
+charList = "[EE<E<E><E>>][E<EE><EE<E><E>>]"
+dockerWriter(charList)
